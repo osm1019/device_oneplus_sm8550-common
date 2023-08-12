81feb3d7b30636656a2937aee7da0699dad096d3
Pranav Vashi <neobuddy89@gmail.com>
2023-08-13 00:54:49 +0530

    sm8550-common: Move few kernel scripts to device tree
    * The oplus modifications are no-op. It's better to have working
      qcom implementation instead.
    * This also allows to further fine-tune.
    
    Change-Id: Iaa7634f6d5bb25035d09bbb4455553718792cff3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -238,6 +238,8 @@ PRODUCT_PACKAGES += \
     fstab.default \
     fstab.default.vendor_ramdisk \
     init.class_main.sh \
+    init.kernel.post_boot.sh \
+    init.kernel.post_boot-kalama.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
@@ -247,6 +249,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.qti.kernel.rc \
     init.target.rc \
     ueventd.oplus.rc \
     ueventd.qcom.rc
