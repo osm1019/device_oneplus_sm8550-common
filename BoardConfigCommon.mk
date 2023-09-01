0a877e8f94cfbb519ac3ab055aa0b00f31182d7e
chandu078 <chandudyavanapelli03@gmail.com>
2023-09-01 08:05:48 +0200

    sm8550-common: Include vendor/oplus/kalama_GKI.config fragment
    Change-Id: Ibcb995e49f518a6e1dd3864cc8b10fe739ce8a7d
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,7 +132,10 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image
 
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8550
-TARGET_KERNEL_CONFIG := salami_defconfig
+TARGET_KERNEL_CONFIG := \
+    gki_defconfig \
+    vendor/kalama_GKI.config \
+    vendor/oplus/kalama_GKI.config
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
